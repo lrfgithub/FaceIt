6c431972d92e53397ef2924f086e5566de17ebb7
carolinecy <caroline.liu.cmu@gmail.com>
2023-08-11 17:29:12 -0700

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Carocyliu on 8/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
